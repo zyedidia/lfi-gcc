ec0ce330dc47e2d9474942f7e6d3ed2da0ad73d5
Zachary Yedidia <zyedidia@gmail.com>
2024-05-08 23:05:11 +0000

    Patches for gcc jump tables

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ sed -i -e 's/aarch64 | aarch64_be/aarch64 | aarch64_be | aarch64_lfi/g' gcc/conf
 sed -i -e 's/aarch64-\*-\*/aarch64-*-* | aarch64_lfi-*-*/g' gcc/libstdc++-v3/configure
 
 patch -p0 < aarch64.cc.patch
+patch -p0 < aarch64.cc.2.patch
+patch -p0 < aarch64-elf.h.patch
 
 cd gcc
 ./contrib/download_prerequisites
