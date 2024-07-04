0c22f8d7b42e5ed6538ff5b03b4666a7abe8d0de
Zachary Yedidia <zyedidia@gmail.com>
2024-07-04 00:19:54 +0000

    Add -z separate-code flag

--- specgen.sh
+++ specgen.sh
@@ -12,12 +12,19 @@ cat << EOM
 @assembler:
 %{!M:%{!MM:%{!E:%{!S:lfi-leg $LFIFLAGS -a $LFIARCH %i | as %(asm_debug) %(asm_options) %A }}}}
 
+@assembler-with-cpp:
+%(trad_capable_cpp) -lang-asm %(cpp_options) -fno-directives-only %{E|M|MM:%(cpp_debug_options)} %{!M:%{!MM:%{!E:%{!S:-o %|.s |
+   lfi-leg $LFIFLAGS -a $LFIARCH %m.s | as %(asm_debug) %(asm_options) %A }}}}
+
 *invoke_as:
 %{!fwpa*:   %{fcompare-debug=*|fdump-final-insns=*:%:compare-debug-dump-opt()}   %{!S:-o %|.s |
  lfi-leg $LFIFLAGS -a $LFIARCH %m.s | as %(asm_options) %A }  }
 
 *cc1:
 + $(lfi-leg -a $LFIARCH --flags=gcc $LFIFLAGS) -fPIC -fno-plt
+
+*link:
++ -z separate-code
 
 *self_spec:
 %{!shared:%{!static:%{!static-pie:-static-pie}}}
